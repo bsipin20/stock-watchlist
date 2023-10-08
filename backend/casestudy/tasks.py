0e40cea7816554741251fc0d912590d0a56051cb
brian <bsipin@gmail.com>
2023-10-08 19:52:16 -0400

    fix

--- backend/casestudy/tasks.py
+++ backend/casestudy/tasks.py
@@ -2,16 +2,14 @@ import os
 import time
 
 from celery import Celery
-
+from casestudy import db, resource
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
-
-   
-   
 @celery.task(name="create_task")
 def create_task(task_type):
-    time.sleep(int(task_type) * 10)
+    testdata = resource.TestAlbertStockClient().get_stock_list()
+    print(testdata)
     return True
