3907592293b9a0518dfc6bafe37b8d27237af8c2
brian <bsipin@gmail.com>
2023-10-07 20:20:37 -0400

    working routes

--- backend/casestudy/routes.py
+++ backend/casestudy/routes.py
@@ -7,6 +7,6 @@ ROUTES = {
     '/v1/users/<int:userId>/watch_list': (users.get_users_watch_list, ['GET']),
     '/v1/users/<int:userId>/watch_list/<string:ticker>': (users.post_users_watch_list, ['POST', 'DELETE']),
     '/v1/securities/search': (securities.search_securities, ['GET']),
-    '/v1/securities/': (securities.get_securities, ['GET']),
+    '/v1/securities': (securities.get_securities, ['GET']),
     '/v1/auth/': (auth.authenticate, ['POST'])
 }
