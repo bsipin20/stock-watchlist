4ca446ef10ce4f89652ee04315e09948b30a3665
brian <bsipin@gmail.com>
2023-10-08 15:02:41 -0400

    client

---
+++ client/src/Watchlist.jsx
@@ -0,0 +1,47 @@
+
+import React, { useState, useEffect, useContext } from 'react'
+import { UserContext } from './UserContext';
+
+function Stock(props) {
+    return (
+      <div className='stock'>
+        <h3>{props.ticker}</h3>
+        <p>{props.name}</p>
+        <p>{props.last_price}</p>
+      </div>
+    )
+}
+
+export function Watchlist(props) {
+    const {user} = useContext(UserContext);
+  
+    if (!user) return null;
+    const [watchlist, setWatchlist] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      fetch('http://localhost:8000/v1/users/1/watch_list', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        setWatchlist(data);
+        console.info('Watchlist loaded successfully');
+      })
+      .catch((error) => {
+        console.error('Unable to load watchlist', error);
+      });
+    }, []);
+  
+    return (
+      <div className='watchlist'>
+        <h3>Watchlist</h3>
+        {("watch_list" in watchlist) && (watchlist["watch_list"] && watchlist["watch_list"].length)
+        ? watchlist["watch_list"].map((stock,index) => (
+          <Stock ticker={stock.ticker} last_price={stock.last_price} name={stock.name} />
+        )) : <p>Empty watchlist</p>}
+      </div>
+    )
+  }
