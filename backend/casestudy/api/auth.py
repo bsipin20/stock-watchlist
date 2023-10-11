0f9e66dffc6521836dfb4ecce2d5e1f79cb6c1ba
brian <bsipin@gmail.com>
2023-10-10 20:53:13 -0400

    working wauth

--- backend/casestudy/api/auth.py
+++ backend/casestudy/api/auth.py
@@ -1,4 +1,16 @@
+import logging
+import sys
+
 from flask import request, jsonify
+from casestudy.extensions import db
+from casestudy.database import User
+
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+)
 
 user = {
         'username': 'user',
@@ -7,6 +19,56 @@ user = {
         'last_name': 'Doe',
 }
 
-def authenticate():
-    query = request.args.get('username', '')  # Get the 'query' parameter from the URL
-    return jsonify(user)
+@jwt_required(locations=['headers'])
+def test_protected_route():
+    return jsonify({'msg': 'You are authorized to view this page!'})        
+
+def login():
+    data = request.form
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify({"msg": "Missing username or password"}), 401
+    user = User.query.filter_by(username=username).first()
+    if user and user.verify_password(password):
+        response = jsonify({"msg": "login successful"})
+        access_token = create_access_token(identity=user.id)
+        set_access_cookies(response, access_token)
+        return response, 200
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+def register():
+    data = request.form
+    username = data.get('username')
+    password = data.get('password')
+
+    try:
+        # Create a new User instance
+        new_user = User(username=username, password=password)
+        logging.info(new_user)
+        # Validate the User instance (this will trigger the @validates decorator in the User model)
+        # This will raise a ValueError if the validation fails
+        db.session.add(new_user)
+        db.session.commit()
+        access_token = create_access_token(identity=user)
+        response = jsonify({"message": "User registered successfully"})
+        set_access_cookies(response, access_token)
+        return response
+    except ValueError as e:
+        raise e
+        # Handle validation errors
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        raise e
+        # Handle other exceptions
+        return jsonify({"error": str(e)}), 500
+ 
+
+
+    return jsonify({"msg": "register successful"})
--- backend/casestudy/database.py
+++ backend/casestudy/database.py
@@ -1,15 +1,41 @@
 from datetime import datetime
 from sqlalchemy.sql import func
+from sqlalchemy.orm import validates
+from werkzeug.security import generate_password_hash, check_password_hash
 from casestudy.extensions import db
 
 class User(db.Model):
     """Model for a user"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        # Hash the password before storing it
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        # Verify the hashed password
+        return check_password_hash(self.password_hash, password)
+
+    @validates('username')
+    def validate_username(self, key, username):
+        # Perform any username validation if needed
+        # For example, ensure it's a valid format
+        # You can customize this validation based on your requirements
+        # This is a simple example, and you can expand on it
+        # For now, we'll just check if the length is at least 3 characters
+        if len(username) < 3:
+            raise ValueError('Username must be at least 3 characters long')
+        return username
 
 class Security(db.Model):
     """Model for a security"""
