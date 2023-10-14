8725257b48d19d2e9bc3a5bd470ab1b59b2a86ef
brian <bsipin@gmail.com>
2023-10-14 12:43:17 -0400

    dont use this route get rid of

--- backend/casestudy/api/routes.py
+++ backend/casestudy/api/routes.py
@@ -5,6 +5,5 @@ ROUTES = {
     '/v1/users/<int:userId>/watch_list': (users.get_users_watch_list, ['GET']),
     '/v1/users/<int:userId>/watch_list/': (users.post_users_watch_list, ['POST', 'DELETE']),
     '/v1/securities/search': (securities.search_securities, ['GET']),
-    '/v1/securities': (securities.get_securities, ['GET']),
     '/v1/login/': (auth.login, ['POST'])
 }
